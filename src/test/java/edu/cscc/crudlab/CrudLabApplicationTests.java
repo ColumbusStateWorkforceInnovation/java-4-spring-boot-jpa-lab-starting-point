86ff7f788c8d0eb5bb28b49bfb2b0f2a1324cc3b
Jim Kirkbide <jkirkbride@cscc.edu>
2024-03-17 14:02:03 -0400

    Initial commit.

---
+++ src/test/java/edu/cscc/crudlab/CrudLabApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.cscc.crudlab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudLabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
